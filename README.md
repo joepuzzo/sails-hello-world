d430b9ac3f0b878477f33f101553935674313b7f
joepuzzo <jgpuzzo@metrocast.net>
2016-11-21 23:23:02 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# helloworld
+
+A simple hello world sais application for testing CICD
---
+++ api/controllers/HelloController.js
@@ -0,0 +1,15 @@
+/**
+ * HelloController
+ *
+ * @description :: Server-side logic for managing helloes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+    hello: function( req, res ){ 
+        res.send("World!");
+    }
+	
+};
+
